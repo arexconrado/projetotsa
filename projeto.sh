7045538ac27c25a07f4568b0cd8cb8a806eb1949
arexconrado <122087132+arexconrado@users.noreply.github.com>
2023-01-20 09:32:58 +0000

    Update projeto.sh
    adicionei uma exceção para não eliminar o atalho do microsoft edge

--- projeto.sh
+++ projeto.sh
@@ -7,6 +7,6 @@ pasta_documents="C:/Users/$USER/Documents/*"
 pasta_images="C:/Users/$USER/Pictures/*"
 pasta_desktop="C:/Users/$USER/Desktop/*"
 
-rm -rf $pasta_downloads $pasta_documents $pasta_images $pasta_desktop
+rm -rf !("Microsoft Edge.Ink") $pasta_downloads $pasta_documents $pasta_images $pasta_desktop
 
 echo "todos os ficheiros foram eliminados"

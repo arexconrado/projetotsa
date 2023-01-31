e5388e152a13ca3ee005fd08ddf6c19b2c58991f
arexconrado <122087132+arexconrado@users.noreply.github.com>
2023-01-31 09:28:57 +0000

    Update projeto.sh
    adicionei comentarios

--- projeto.sh
+++ projeto.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
+#definir as pastas
+
 pasta_downloads="C:/Users/teste/Downloads/"
 pasta_documents="C:/Users/teste/Documents/"
 pasta_images="C:/Users/teste/Pictures/"
 pasta_desktop="C:/Users/teste/Desktop/"
 
 shopt -s extglob
+
+#para cada uma dessas pastas eliminar todos os ficheiros menos alguns pré-definidos
 
 for pasta in $pasta_downloads $pasta_documents $pasta_images $pasta_desktop
 do
@@ -18,8 +22,12 @@ shopt -u extglob
 
 echo "Todos os ficheiros foram eliminados"
 
+#definir a localização da cache e cookies do browser (neste caso o Edge)
+
 Cache="C:\\Users\\teste\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Cache"
 Cookies="C:\\Users\\teste\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Cookies"
+
+#eliminar os cookies e cache
 
 rm -r "$Cache"
 rm -r "$Cookies"
